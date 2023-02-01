fb95dab1d4c7cbdeeec6e149801412b5a3aa8868
Divyanshu Urmaliya <urmaliyadiv04@gmail.com>
2023-02-01 21:53:15 +0530

    Added my style

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (e) => {
+    // console.log(e.target.value);
+    setUserInput(e.target.value);
+  };
   return (
     <div className="root">
       <Head>
@@ -11,11 +17,22 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>food mood generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>food for every mood - tell us what it is!</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput}
+           onChange={onUserChangedText} />
+           <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Generate</p>
+            </div>
+          </a>
+        </div>
         </div>
       </div>
       <div className="badge-container grow">
